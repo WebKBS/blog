c8f945cacf0602d2109963470750c5f3308bfe50
gangbeomsu <kbs21c1987@gmail.com>
2024-03-15 17:44:23 +0900

    docker 셋팅

---
+++ config/defaultData.ts
@@ -0,0 +1,11 @@
+export const defaultData = {
+  title: 'StoryLog',
+  description: '나만의 개발 블로그, 기록 또 기록',
+  url: 'https://storylog.kr',
+  author: 'devK',
+  email: 'dev21c2020@gmail.com',
+  links: {
+    github: '',
+    gitbook: '',
+  },
+};
