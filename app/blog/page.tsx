d5e4666e6bcb4b5c8e2d467d0e076e45a47822e7
gangbeomsu <kbs21c1987@gmail.com>
2024-03-17 17:13:43 +0900

    Dummy data 추가

---
+++ app/blog/page.tsx
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: 'blog',
+  description: '블로그 홈',
+};
+
+const BlogPage = () => {
+  return <section className="container">BlogPage</section>;
+};
+
+export default BlogPage;
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import ScrollProgress from '@/components/ScrollProgress';
 
 export default function Home() {
   return (
-    <div className="h-[5000px]">
+    <div className="container">
       <ScrollProgress />
       <h1>Home</h1>
     </div>
