d831966681d00929e92ebd7c0d0405f04aaf07bb
gangbeomsu <kbs21c1987@gmail.com>
2024-03-19 00:38:28 +0900

    slug 페이지 에러 원인 시점

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -1,5 +1,33 @@
-const BlogDetail = () => {
-  return <div>BlogDetail</div>;
+import { posts } from '#site/content';
+
+interface BlogDetailProps {
+  params: {
+    slug: string[];
+  };
+}
+
+const getPost = (params: BlogDetailProps['params']) => {
+  const slug = params?.slug?.join('/');
+  const post = posts.find((post) => post.permalink === '/blog/' + slug);
+
+  // console.log('내부: ', post);
+
+  return post;
+};
+
+// https://nextjs.org/docs/app/api-reference/functions/generate-static-params
+// 빌드 시점에 정적으로 생성할 페이지의 경로를 반환.
+// export const generateStaticParams = () => {}
+
+const BlogDetail = ({ params: { slug } }: BlogDetailProps) => {
+  const post = getPost({ slug });
+  // console.log(post);
+
+  return (
+    <section className="py-12 max-w-screen-lg px-6 mx-auto">
+      <h2>{post.title}</h2>
+    </section>
+  );
 };
 
 export default BlogDetail;
