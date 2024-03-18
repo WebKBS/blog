81838ea9716971f2c3a1aaf7e2b1cbdcff955851
gangbeomsu <kbs21c1987@gmail.com>
2024-03-18 21:02:46 +0900

    레이아웃 1차완료 레이아웃 수정

--- components/LinkCard/LinkCard.tsx
+++ components/LinkCard/LinkCard.tsx
@@ -24,7 +24,7 @@ const LinkCard = ({
     <li>
       <Link
         href={`/${slug}`}
-        className="border rounded-md flex flex-col overflow-hidden px-4 py-4 gap-4 sm:flex-row"
+        className="border rounded-md flex flex-col overflow-hidden px-4 py-4 gap-4 sm:flex-row-reverse"
       >
         <div className="w-full relative pt-[100%] overflow-hidden sm:pt-0 sm:w-44 sm:h-44">
           {cover ? (
@@ -37,12 +37,16 @@ const LinkCard = ({
             />
           ) : null}
         </div>
-        <div className="sm:flex-1 flex flex-col justify-between gap-4">
+        <div className="flex flex-col justify-between gap-4 sm:flex-1">
           <div>
-            <h2 className="text-xl font-semibold">{title}</h2>
-            <p className="line-clamp-1">{description}</p>
+            <h2 className="text-xl font-semibold line-clamp-2 mb-2 md:mb-4">
+              {title}
+            </h2>
+            <p className="line-clamp-2 md:line-clamp-3 text-sm">
+              {description}
+            </p>
           </div>
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-3">
             <ul className="flex flex-wrap gap-1">
               {tags.map((tag) => (
                 <li key={tag}>
@@ -50,7 +54,7 @@ const LinkCard = ({
                 </li>
               ))}
             </ul>
-            <time className="self-end text-sm" dateTime={date}>
+            <time className="self-end text-sm sm:self-start" dateTime={date}>
               {formatDate(date)}
             </time>
           </div>
