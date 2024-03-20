3c603d576653696a4e1c3f32e4d2efb16f25af86
gangbeomsu <kbs21c1987@gmail.com>
2024-03-20 23:05:30 +0900

    메모제이션 취소

--- components/TagsBadge/TagsBadge.tsx
+++ components/TagsBadge/TagsBadge.tsx
@@ -1,6 +1,7 @@
 'use client';
+
 import { useTagStore } from '@/store/tagStore';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Badge } from '../ui/badge';
 
 interface TagBadgeProps {
@@ -11,19 +12,17 @@ const TagBadge = ({ tags }: TagBadgeProps) => {
   const tagState = useTagStore((state) => state.tagState);
   const [tagStyle, setTagStyle] = useState('');
 
-  const memoizedTags = useMemo(() => tags, [tags]);
-
   useEffect(() => {
-    if (memoizedTags.includes(tagState)) {
+    if (tags.includes(tagState)) {
       setTagStyle(tagState);
     } else {
       setTagStyle('default');
     }
-  }, [tagState, memoizedTags]);
+  }, [tagState, tags]);
 
   return (
     <>
-      {memoizedTags.map((tag) => (
+      {tags.map((tag) => (
         <li key={tag}>
           <Badge variant={tag === tagStyle ? 'default' : 'secondary'}>
             {tag}
