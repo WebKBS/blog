d9c698751d563bf800adc660bc66d0683ad7d955
gangbeomsu <kbs21c1987@gmail.com>
2024-03-20 14:33:24 +0900

    다이나믹 라우트 og 태그 api 추가

---
+++ app/api/og/route.tsx
@@ -0,0 +1,91 @@
+// https://nextjs.org/docs/app/api-reference/file-conventions/metadata/opengraph-image#generate-images-using-code-js-ts-tsx
+// og:image를 생성하는 API 라우트 공식 문서 참고
+
+import { defaultData } from '@/config/defaultData';
+import { ImageResponse } from 'next/og';
+import { NextRequest } from 'next/server';
+
+export const runtime = 'edge';
+
+const interBold = fetch(
+  new URL('../../../assets/fonts/Inter-Bold.ttf', import.meta.url)
+).then((res) => res.arrayBuffer());
+
+export async function GET(req: NextRequest) {
+  try {
+    const fontBold = await interBold;
+
+    const { searchParams } = req.nextUrl;
+    const title = searchParams.get('title');
+
+    if (!title) {
+      return new Response('title을 입력해주세요', { status: 400 });
+    }
+
+    const heading = title.length > 40 ? `${title.substring(0, 140)}...` : title;
+
+    return new ImageResponse(
+      (
+        <div tw="flex relative flex-col p-12 w-full h-full items-start text-black bg-white">
+          <div tw="flex items-center">
+            <svg
+              width="40"
+              height="40"
+              viewBox="0 0 40 40"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g clip-path="url(#clip0_21_26)">
+                <path
+                  d="M0 2.5C0 1.11929 1.11929 0 2.5 0H37.5C38.8807 0 40 1.11929 40 2.5V37.5C40 38.8807 38.8807 40 37.5 40H2.5C1.11929 40 0 38.8807 0 37.5V2.5Z"
+                  fill="#7F00FF"
+                />
+                <path
+                  d="M36 20C36 28.8366 28.8366 36 20 36C11.1634 36 4 28.8366 4 20C4 11.1634 11.1634 4 20 4C28.8366 4 36 11.1634 36 20Z"
+                  fill="white"
+                />
+                <path
+                  d="M9 31V9H20.1918C22.2203 9 23.9709 9.2972 25.4438 9.8916C26.9254 10.4788 28.0675 11.3239 28.8701 12.4268C29.6727 13.5225 30.074 14.8223 30.074 16.3262C30.074 17.8516 29.6639 19.1478 28.8437 20.2148C28.0235 21.2747 26.8593 22.084 25.3512 22.6426C23.8431 23.194 22.0571 23.4697 19.9934 23.4697H12.9158V19.2803H18.7763C19.7641 19.2803 20.5887 19.1764 21.2502 18.9688C21.9204 18.7539 22.4275 18.4316 22.7715 18.002C23.1155 17.5651 23.2874 17.0065 23.2874 16.3262C23.2874 15.6458 23.1155 15.0837 22.7715 14.6396C22.4275 14.1885 21.9204 13.8519 21.2502 13.6299C20.5799 13.4007 19.7553 13.2861 18.7763 13.2861H15.5484V31H9ZM24.2532 20.9453L31 31H23.8563L17.2417 20.9453H24.2532Z"
+                  fill="#7F00FF"
+                />
+              </g>
+              <defs>
+                <clipPath id="clip0_21_26">
+                  <rect width="40" height="40" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+
+            <p tw="ml-2 font-bold text-2xl">RecodeLog</p>
+          </div>
+          <div tw="flex flex-col flex-1 py-10">
+            <div tw="flex text-xl uppercase font-bold tracking-tight font-normal">
+              BLOG POST
+            </div>
+            <div tw="flex text-[80px] font-bold text-[50px]">{heading}</div>
+          </div>
+          <div tw="flex items-center w-full justify-between">
+            <div tw="flex text-2xl">{defaultData.url}</div>
+            <div tw="flex items-center text-2xl">
+              <div tw="flex ml-2">{defaultData.links.github}</div>
+            </div>
+          </div>
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+        fonts: [
+          {
+            name: 'Inter',
+            data: fontBold,
+            style: 'normal',
+            weight: 700,
+          },
+        ],
+      }
+    );
+  } catch (error) {
+    return new Response('이미지 만들기 실패', { status: 500 });
+  }
+}
--- config/defaultData.ts
+++ config/defaultData.ts
@@ -1,7 +1,7 @@
 export const defaultData = {
   title: 'RecodeLog',
   description: '나만의 개발 블로그, 기록 또 기록',
-  url: 'https://Recodelog.kr',
+  url: 'https://recodelog.com',
   author: 'devK',
   email: 'dev21c2020@gmail.com',
   links: {
