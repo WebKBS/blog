58a1d3d54c29f092f10e75608f25e985aad999e2
gangbeomsu <kbs21c1987@gmail.com>
2024-03-20 14:45:38 +0900

    github 아이콘 추가및 다이나믹 라우트 메타데이터 설정

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -1,8 +1,10 @@
 import { posts } from '#site/content';
 import ScrollProgress from '@/components/ScrollProgress';
 import { MDXContent } from '@/components/mdx-content';
+import { defaultData } from '@/config/defaultData';
 import { formatDate } from '@/lib/utils';
 import '@/styles/mdx.css';
+import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 
 interface BlogDetailProps {
@@ -19,6 +21,43 @@ const getPost = (params: BlogDetailProps['params']) => {
 
   return post;
 };
+
+export async function generateMetadata({
+  params,
+}: BlogDetailProps): Promise<Metadata> {
+  const post = await getPost(params);
+
+  if (!post || !post.published) {
+    return {};
+  }
+
+  const ogSearchParams = new URLSearchParams();
+  ogSearchParams.set('title', post.title);
+
+  return {
+    title: post.title,
+    description: post.description,
+    authors: { name: defaultData.author },
+    openGraph: {
+      title: post.title,
+      description: post.description,
+      type: 'article',
+      url: `${defaultData.url}/blog/${post.permalink}`,
+      images: {
+        url: `${defaultData.url}/api/og?${ogSearchParams.toString()}`,
+        width: 1200,
+        height: 630,
+        alt: post.title,
+      },
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: post.title,
+      description: post.description,
+      images: [`/api/og?${ogSearchParams.toString()}`],
+    },
+  };
+}
 
 // https://nextjs.org/docs/app/api-reference/functions/generate-static-params
 // 빌드 시점에 정적으로 생성할 페이지의 경로를 반환.
