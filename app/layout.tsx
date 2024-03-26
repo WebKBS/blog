500c4dd512072a1e03f8c21c024a8b28fb5722d1
gangbeomsu <kbs21c1987@gmail.com>
2024-03-26 18:31:45 +0900

    metadata 작성중

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
   },
 
   icons: {
-    icon: './icons/icon.png',
+    icon: './icons/icon.svg',
     apple: './icons/apple-icon.png',
     shortcut: './icons/icon.png',
   },
