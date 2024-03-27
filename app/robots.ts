c376a1ebe5fa547d0119f2752f766fe3ecca2b23
gangbeomsu <kbs21c1987@gmail.com>
2024-03-27 21:02:17 +0900

    로고 수정 및 robots 수정, 블로그 작업중

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,5 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
     },
     sitemap: 'https://recodelog.com/sitemap.xml',
-    host: 'https://recodelog.com',
   };
 }
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -10,9 +10,11 @@ const Header = () => {
     <header className="sticky left-0 top-0 z-50 flex h-14 items-center border-b px-5 py-2 backdrop-blur-xl">
       <div className="flex w-full items-center justify-between max-w-screen-xl mx-auto">
         <SideMenuButton className="md:hidden" />
-        <p className="text-xl font-bold uppercase flex items-center gap-2">
-          <Image src="/logo.png" alt="RecodeLog" width={32} height={32} />
-          <Link href="/">RecodeLog</Link>
+        <p className="text-xl font-bold uppercase ">
+          <Link href="/" className="flex items-center gap-2">
+            <Image src="/logo.png" alt="RecodeLog" width={32} height={32} />
+            RecodeLog
+          </Link>
         </p>
         <nav className="hidden ml-auto md:block">
           <Navigation className="mr-8 hidden font-semibold md:flex md:items-center md:gap-6" />
