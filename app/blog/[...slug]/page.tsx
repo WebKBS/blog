b22eaedefdaffcbd65a6e4a198171833d54331f2
gangbeomsu <kbs21c1987@gmail.com>
2024-04-06 21:49:40 +0900

    SSG 페이지 에러 수정 및 loading 추가

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -66,10 +66,9 @@ export async function generateMetadata({
 
 // https://nextjs.org/docs/app/api-reference/functions/generate-static-params
 // 빌드 시점에 정적으로 생성할 페이지의 경로를 반환.
-export const generateStaticParams = async (): Promise<
-  BlogDetailProps['params'][]
-> => {
+export const generateStaticParams = () => {
   // posts 배열에서 published가 true인 것만 필터링하여 slug만 반환
+
   return posts
     .filter((post) => post.published)
     .map((post) => ({
---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+const loading = () => {
+  return (
+    <section className="pb-24 pt-12 max-w-screen-lg px-6 mx-auto">
+      데이터 가져오는 중..
+    </section>
+  );
+};
+
+export default loading;
