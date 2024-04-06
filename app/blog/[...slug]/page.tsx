b09a4d58febc96bccd2f194948e262f92f86fcb4
gangbeomsu <kbs21c1987@gmail.com>
2024-04-06 22:37:13 +0900

    generateStaticParams 에러 완전 수정

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -66,15 +66,23 @@ export async function generateMetadata({
 
 // https://nextjs.org/docs/app/api-reference/functions/generate-static-params
 // 빌드 시점에 정적으로 생성할 페이지의 경로를 반환.
-export const generateStaticParams = () => {
+export const generateStaticParams = async () => {
   // posts 배열에서 published가 true인 것만 필터링하여 slug만 반환
 
   return posts
     .filter((post) => post.published)
     .map((post) => ({
-      slug: post.permalink.split('/').slice(2),
+      slug: post.permalink.split('/'),
     }));
 };
+
+// console.log(
+//   posts
+//     .filter((post) => post.published)
+//     .map((post) => ({
+//       slug: post.permalink.split('/'),
+//     }))
+// );
 
 const BlogDetail = async ({ params: { slug } }: BlogDetailProps) => {
   const post = await getPost({ slug });
