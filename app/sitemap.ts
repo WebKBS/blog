0122bb1b32f7d0ee1196129dcb0e9b4f3eb1004d
gangbeomsu <kbs21c1987@gmail.com>
2024-04-21 14:19:07 +0900

    sitemap 수정

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,7 +6,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     url: `https://recodelog.com/${post.slug}`,
     lastModified: new Date(post.date).toISOString().split('T')[0],
     changeFrequency: 'weekly',
-    priority: index === posts.length - 1 ? 1 : 0.8,
+    priority: index === posts.length - 1 ? 0.9 : 0.7,
   }));
 
   return [
@@ -14,11 +14,13 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: 'https://recodelog.com', // 사용자가 접근할 수 있는 URL
       lastModified: new Date().toISOString().split('T')[0], // 마지막으로 수정된 날짜
       changeFrequency: 'weekly', // 변경 빈도
+      priority: 1,
     },
     {
       url: 'https://recodelog.com/blog',
       lastModified: new Date().toISOString().split('T')[0],
       changeFrequency: 'weekly',
+      priority: 0.8,
     },
     ...post,
   ];
