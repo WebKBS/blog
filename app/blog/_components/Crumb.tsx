8bed9a0111243984e48c3589b98d98a87cfddca4
gangbeomsu <kbs21c1987@gmail.com>
2024-06-01 23:23:12 +0900

    이미지 컴포넌트 블로그 추가. 수정 예정

--- app/blog/_components/Crumb.tsx
+++ app/blog/_components/Crumb.tsx
@@ -8,7 +8,7 @@ import {
 import Link from 'next/link';
 const Crumb = ({ title }: { title: string }) => {
   return (
-    <div className="sticky top-14 left-0 z-10 backdrop-blur-2xl py-2">
+    <div className="sticky top-14 left-0 z-10 backdrop-blur-2xl py-2 bg-background/75">
       <div className="max-w-screen-lg mx-auto  px-6">
         <Breadcrumb>
           <BreadcrumbList className="flex-nowrap">
@@ -24,7 +24,7 @@ const Crumb = ({ title }: { title: string }) => {
               </BreadcrumbLink>
             </BreadcrumbItem>
             <BreadcrumbSeparator />
-            <BreadcrumbItem className="line-clamp-1 flex-1 text-white">
+            <BreadcrumbItem className="line-clamp-1 flex-1 text-black dark:text-white">
               {title}
             </BreadcrumbItem>
           </BreadcrumbList>
