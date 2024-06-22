0062bd239ff668b746889c0178538bfe78253d7e
gangbeomsu <kbs21c1987@gmail.com>
2024-06-22 22:40:44 +0900

    middleware 수정

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,10 +1,18 @@
-import type { NextRequest } from 'next/server';
-import { NextResponse } from 'next/server';
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/', request.url));
+  const response = NextResponse.next();
+
+  // X-Frame-Options 헤더 설정
+  response.headers.set("X-Frame-Options", "DENY");
+
+  // Content-Security-Policy 헤더 설정
+  response.headers.set("Content-Security-Policy", "frame-ancestors 'none'");
+
+  return response;
 }
 
 export const config = {
-  matcher: '/api/:path*',
+  matcher: "/:path*",
 };
