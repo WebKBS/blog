0d3bf855f9268fe7bd53c81054de9cd35038b91a
kbs21c1987 <kbs21c1987@gmail.com>
2024-10-03 12:53:25 +0900

    fix : 버튼 접근성 추가

--- components/Buttons/ScrollUpButton.tsx
+++ components/Buttons/ScrollUpButton.tsx
@@ -1,14 +1,14 @@
-"use client";
-import React from "react";
-import { ArrowUpToLine } from "lucide-react";
+'use client';
+import { ArrowUpToLine } from 'lucide-react';
 
 const ScrollUpButton = () => {
   return (
     <button
       className="fixed bottom-4 right-4 xl:bottom-8 xl:right-8 z-10 bg-gray-400 px-2 py-2 rounded-full"
       onClick={() => {
-        window.scrollTo({ top: 0, behavior: "smooth" });
+        window.scrollTo({ top: 0, behavior: 'smooth' });
       }}
+      aria-label="상단으로 이동"
     >
       <ArrowUpToLine color="#000" size={20} />
     </button>
