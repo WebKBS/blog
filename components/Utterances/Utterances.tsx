bff98a6e03d4d10693d5c7fe1f795b1d2f28a634
kbs21c1987 <kbs21c1987@gmail.com>
2024-10-26 20:06:45 +0900

    fix: Utterances -> Giscus로 변경

--- components/Utterances/Utterances.tsx
+++ components/Utterances/Utterances.tsx
@@ -1,38 +1,75 @@
-"use client";
-import React, { useEffect, useRef } from "react";
-import useThemeMode from "@/hooks/useThemeMode";
+'use client';
+import useThemeMode from '@/hooks/useThemeMode';
+import { useEffect, useRef } from 'react';
 
 const Utterances = () => {
   const isLightTheme = useThemeMode(); // 커스텀 훅 사용
   const ref = useRef<HTMLDivElement>(null);
 
+  // useEffect(() => {
+  //   // Utterances가 이미 렌더링 되었는지 확인
+  //   const existingScript = ref.current?.querySelector<HTMLIFrameElement>(
+  //     'iframe.utterances-frame'
+  //   );
+
+  //   // if (!existingScript) {
+  //   //   // Utterances 스크립트가 없으면 추가
+  //   //   const script = document.createElement("script");
+  //   //   script.src = "https://utteranc.es/client.js";
+  //   //   script.async = true;
+  //   //   script.setAttribute("repo", "WebKBS/github-comments");
+  //   //   script.setAttribute("issue-term", "title");
+  //   //   script.setAttribute(
+  //   //     "theme",
+  //   //     isLightTheme ? "github-light" : "github-dark",
+  //   //   );
+  //   //   script.setAttribute("label", "comments");
+  //   //   script.setAttribute("crossorigin", "anonymous");
+  //   //   ref.current?.appendChild(script);
+  //   // } else {
+  //   //   // 이미 존재하면 테마만 변경
+  //   //   const message = {
+  //   //     type: "set-theme",
+  //   //     theme: isLightTheme ? "github-light" : "github-dark",
+  //   //   };
+  //   //   existingScript.contentWindow?.postMessage(message, "https://utteranc.es");
+  //   // }
+  // }, [isLightTheme]);
+
   useEffect(() => {
-    // Utterances가 이미 렌더링 되었는지 확인
+    // giscus가 이미 렌더링 되었는지 확인
     const existingScript = ref.current?.querySelector<HTMLIFrameElement>(
-      "iframe.utterances-frame",
+      'iframe.giscus-frame'
     );
 
-    if (!existingScript) {
-      // Utterances 스크립트가 없으면 추가
-      const script = document.createElement("script");
-      script.src = "https://utteranc.es/client.js";
-      script.async = true;
-      script.setAttribute("repo", "WebKBS/github-comments");
-      script.setAttribute("issue-term", "title");
-      script.setAttribute(
-        "theme",
-        isLightTheme ? "github-light" : "github-dark",
-      );
-      script.setAttribute("label", "comments");
-      script.setAttribute("crossorigin", "anonymous");
-      ref.current?.appendChild(script);
-    } else {
-      // 이미 존재하면 테마만 변경
+    if (existingScript) {
+      // 이미 존재하는 경우 메시지로 테마만 변경
       const message = {
-        type: "set-theme",
-        theme: isLightTheme ? "github-light" : "github-dark",
+        giscus: {
+          setConfig: {
+            theme: isLightTheme ? 'light' : 'dark_dimmed',
+          },
+        },
       };
-      existingScript.contentWindow?.postMessage(message, "https://utteranc.es");
+      existingScript.contentWindow?.postMessage(message, 'https://giscus.app');
+    } else {
+      // 처음 렌더링하는 경우 스크립트 생성
+      const script = document.createElement('script');
+      script.src = 'https://giscus.app/client.js';
+      script.async = true;
+      script.setAttribute('data-repo', 'WebKBS/github-comments');
+      script.setAttribute('data-repo-id', 'R_kgDOM2084A');
+      script.setAttribute('data-category', 'Comments');
+      script.setAttribute('data-category-id', 'DIC_kwDOM2084M4Cjtd5');
+      script.setAttribute('data-mapping', 'pathname');
+      script.setAttribute('data-strict', '0');
+      script.setAttribute('data-reactions-enabled', '1');
+      script.setAttribute('data-emit-metadata', '0');
+      script.setAttribute('data-input-position', 'bottom');
+      script.setAttribute('data-theme', isLightTheme ? 'light' : 'dark_dimmed');
+      script.setAttribute('data-lang', 'ko');
+      script.setAttribute('crossorigin', 'anonymous');
+      ref.current?.appendChild(script);
     }
   }, [isLightTheme]);
 
