ba186c80f453837ea6ee173374f7e688b4a18c0d
kbs21c1987 <kbs21c1987@gmail.com>
2024-10-26 20:41:39 +0900

    feat: 이미지 스켈레톤 추가

--- components/MDXImage/MDXImage.tsx
+++ components/MDXImage/MDXImage.tsx
@@ -1,3 +1,4 @@
+import { shimmer, toBase64 } from '@/utils/imageSkeleton';
 import Image, { StaticImageData } from 'next/image';
 
 interface IMDXImage {
@@ -19,8 +20,9 @@ const MDXImage = ({ src, alt, width }: IMDXImage) => {
         height={1024}
         alt={alt}
         className="w-auto mb-2 border border-gray-200 dark:border-gray-700 rounded-md"
-        style={width ? { width } : {width: "640px"}}
+        style={width ? { width } : { width: '640px' }}
         quality={100}
+        placeholder={`data:image/svg+xml;base64,${toBase64(shimmer(100, 100))}`}
       />
       <p className="text-center text-gray-500 dark:text-gray-400 m-0 text-sm">
         {alt}
